c45376e748f5c1c11bfad909200964eb9607d8b3
carpanla <carpanla@gmail.com>
2019-12-16 19:54:31 +0100

    add className

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -5,21 +5,21 @@ import Buttons from "Buttons";
 class CardPreview extends React.Component {
     render() {
         return (
-    <section class="preview">
-        <div class="preview__wrapper">
-            <button id="btn-reset" class="btn-reset"><i class="far fa-trash-alt"></i> reset</button>
-            <div class="card select-palette1" id="cardId">
-                <div class="card__id">
-                    <h3 id="cardName" class="card__id--name">Nombre Apellido</h3>
-                    <p id="cardJob" class="card__id--role">Front-end developer</p>
+    <section className="preview">
+        <div className="preview__wrapper">
+            <button id="btn-reset" className="btn-reset"><i className="far fa-trash-alt"></i> reset</button>
+            <div className="card select-palette1" id="cardId">
+                <div className="card__id">
+                    <h3 id="cardName" className="card__id--name">Nombre Apellido</h3>
+                    <p id="cardJob" className="card__id--role">Front-end developer</p>
                 </div>
-                <div class="card__photo js__profile-image"></div>
-                <div class="card__contacts">
+                <div className="card__photo js__profile-image"></div>
+                <div className="card__contacts">
                     <ul>
-                        <a id = "linkPhone" href=""><li id="cardPhone" class="contact-icon contact-icon-palette1 hidden"><i class="fas fa-mobile-alt"></i></li></a>
-                        <a id = "linkEmail" href=""><li id="cardEmail" class="contact-icon contact-icon-palette1 hidden"><i class="far fa-envelope"></i></li></a>
-                        <a id = "linkLinkedin" href="" target="_blank"><li id="cardLinkedin" class="contact-icon contact-icon-palette1 hidden"><i class="fab fa-linkedin-in"></i></li></a>
-                        <a id = "linkGithub" href="" target="_blank"><li id="cardGithub" class="contact-icon contact-icon-palette1 hidden"><i class="fab fa-github-alt"></i></li></a>
+                        <a id = "linkPhone" href=""><li id="cardPhone" className="contact-icon contact-icon-palette1 hidden"><i class="fas fa-mobile-alt"></i></li></a>
+                        <a id = "linkEmail" href=""><li id="cardEmail" className="contact-icon contact-icon-palette1 hidden"><i class="far fa-envelope"></i></li></a>
+                        <a id = "linkLinkedin" href="" target="_blank"><li id="cardLinkedin" className="contact-icon contact-icon-palette1 hidden"><i class="fab fa-linkedin-in"></i></li></a>
+                        <a id = "linkGithub" href="" target="_blank"><li id="cardGithub" className="contact-icon contact-icon-palette1 hidden"><i class="fab fa-github-alt"></i></li></a>
                     </ul>
                 </div>
             </div>
