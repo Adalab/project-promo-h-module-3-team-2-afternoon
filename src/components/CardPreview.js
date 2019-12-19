fe2290e6d21383c1f01649f1fea9d6a86790a49c
Ana Arribas <arribas.ibd@gmail.com>
2019-12-19 21:07:10 +0100

    try to paint inputs

--- src/components/CardPreview.js
+++ src/components/CardPreview.js
@@ -5,17 +5,26 @@ import PreviewIcons from "./PreviewIcons";
 import Buttons from "./Buttons";
 
 class CardPreview extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+    }
+
+
+    onChangeHandler(data) {
+        console.log(data)
+    }
+
     render() {
         return (
             <section className="preview">
                 <div className="preview__wrapper">
-                <Buttons btnClass='btn-reset' text=' reset'>
-                    <i className="far fa-trash-alt"></i>
-                </Buttons>
+                    <Buttons btnClass='btn-reset' text=' reset'>
+                        <i className="far fa-trash-alt"></i>
+                    </Buttons>
                     <div className="card select-palette1" id="cardId">
                         <div className="card__id">
-                            <h3 id="cardName" className="card__id--name">Nombre Apellido</h3>
+                            <h3 id="cardName" className="card__id--name" nombre={this.onChangeHandler}>Nombre Apellido</h3>
                             <p id="cardJob" className="card__id--role">Front-end developer</p>
                         </div>
                         <div className="card__photo js__profile-image">
