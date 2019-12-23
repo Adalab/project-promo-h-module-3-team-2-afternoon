36e41a33a59bba9ee3492928e5f25181acbdecdd
Ana Arribas <arribas.ibd@gmail.com>
2019-12-23 11:47:52 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Landing from './components/Landing'
 import CardPage from './components/CardPage';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <CardPage />
-    )
+      <div>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/cardpage" component={CardPage} />
+          </Switch>
+        </main>
+      </div>
+    );
   }
 }
 export default App;
