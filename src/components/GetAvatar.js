143d5c6d28b8b0d4b0c6ded61ecde44a46923050
mariaadobos <mariyadob@gmail.com>
2020-01-10 17:04:09 +0100

    delete comments

--- src/components/GetAvatar.js
+++ src/components/GetAvatar.js
@@ -1,5 +1,4 @@
 import React from "react";
-//import './GetAvatar.css';
 import PropTypes from 'prop-types';
 
 class GetAvatar extends React.Component {
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-//import './Profile.css';
-
 class Profile extends React.Component {
   render() {
     const {avatar} = this.props;
