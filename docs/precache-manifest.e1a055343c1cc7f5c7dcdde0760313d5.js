8ee56d17a06a85a12403a3991fc3f43a60fcf453
Ana Arribas <arribas.ibd@gmail.com>
2020-01-16 17:11:08 +0100

    Add docs

---
+++ docs/precache-manifest.e1a055343c1cc7f5c7dcdde0760313d5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872da1142add3bac8e3a22459a1fea91",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f09c73c2dbb7a01d2e05",
+    "url": "./static/css/main.55dff5e7.chunk.css"
+  },
+  {
+    "revision": "fb68df9e42efc1705b01",
+    "url": "./static/js/2.ec268221.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.ec268221.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f09c73c2dbb7a01d2e05",
+    "url": "./static/js/main.d3208688.chunk.js"
+  },
+  {
+    "revision": "4f3661a719537d8366cb",
+    "url": "./static/js/runtime-main.4ccbcaca.js"
+  },
+  {
+    "revision": "3fb20e9de6362995ab9da353f62794f5",
+    "url": "./static/media/background-img.3fb20e9d.png"
+  },
+  {
+    "revision": "0118e311c7fc2f113bee1abedecf92b2",
+    "url": "./static/media/download.0118e311.svg"
+  },
+  {
+    "revision": "c6824601c403e5e4b5cf17c4b00d1470",
+    "url": "./static/media/loader.c6824601.gif"
+  },
+  {
+    "revision": "142e0c4aae616c28c00ff6485fef398b",
+    "url": "./static/media/profilePicture.142e0c4a.gif"
+  },
+  {
+    "revision": "6630a87bfab6a050f2c445137c4586cc",
+    "url": "./static/media/remder-logo.6630a87b.gif"
+  }
+]);
